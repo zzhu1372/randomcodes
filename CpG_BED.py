a000ee9f3e0267a459e01002f12764238b7115b6
zzhu1372 <142303647+zzhu1372@users.noreply.github.com>
2025-09-18 15:25:18 +0200

    end+1 for DMRs

--- CpG_BED.py
+++ CpG_BED.py
@@ -8,7 +8,7 @@ def find_overlapping_regions(bed1_df, bed2_df, bed1_cols, bed2_cols):
     bed_2 = BedTool.from_dataframe(bed2_df[bed2_cols].sort_values(bed2_cols))
     return BedTool.to_dataframe(bed_1.intersect(bed_2, wa=True, wb=True))
     
-def CpGs_to_features(met, ref, metcols, refcols, loffset=0, roffset=1):
+def CpGs_to_features(met, ref, metcols, refcols, loffset=0, roffset=1, loffset2=0, roffset2=1):
     # met: datatable
     # ref: pandas
     # metcols: [chrom, pos, other columns that are not samples]
@@ -17,7 +17,10 @@ def CpGs_to_features(met, ref, metcols, refcols, loffset=0, roffset=1):
     cpgpos['start'] = cpgpos[metcols[1]]+loffset
     cpgpos['end'] = cpgpos[metcols[1]]+roffset
 
-    cpgsoi = find_overlapping_regions(ref, cpgpos, refcols, [metcols[0],'start','end'])
+    ref2 = ref.copy()
+    ref2[refcols[1]] = ref2[refcols[1]] + loffset2
+    ref2[refcols[2]] = ref2[refcols[2]] + roffset2
+    cpgsoi = find_overlapping_regions(ref2, cpgpos, refcols, [metcols[0],'start','end'])
     cpgsoi.index = cpgsoi['score'].astype(str)+'@'+(cpgsoi['strand']-loffset).astype(str)
 
     cpgpos['name'] = cpgpos[metcols[0]].astype(str)+'@'+cpgpos['start'].astype(str)
